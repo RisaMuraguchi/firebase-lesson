423e0a61497cbb8b9ea9f273dc771850d0fe2b74
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-24 11:10:58 +0900

    [Add]9-3

--- src/components/LoginExample.vue
+++ src/components/LoginExample.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { reactive } from 'vue';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 
 const data = reactive({
   email: "",
   password: "",
+  newEmail: "",
+  newPassword: "",
 });
 
 const auth = getAuth();
@@ -18,11 +20,23 @@ const login = () => {
       alert("正しいログイン情報を入力してください");
     });
 };
+
+const signUp = () => {
+  createUserWithEmailAndPassword(auth, data.newEmail, data.newPassword)
+    .then((userCredential) => {
+      alert("ユーザー作成に成功しました！");
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("ユーザー作成に失敗しました");
+    });
+};
 </script>
 
 <template>
   <h1>Login Example</h1>
 
+  <h2>ログイン</h2>
   <div class="container">
 
     <div class="input-area">
@@ -35,6 +49,22 @@ const login = () => {
     </div>
     <div class="submit-area">
       <button class="login" @click="login">ログイン</button>
+    </div>
+  </div>
+
+  <h2>新規作成</h2>
+  <div class="container">
+
+    <div class="input-area">
+      <label for="new-email">メールアドレス</label>
+      <input id="new-email" type="text" name="new-email" v-model="data.newEmail">
+    </div>
+    <div class="input-area">
+      <label for="new-password">パスワード</label>
+      <input id="new-password" type="password" name="new-password" v-model="data.newPassword">
+    </div>
+    <div class="submit-area">
+      <button class="signUp" @click="signUp">ユーザーを作成</button>
     </div>
   </div>
 </template>
