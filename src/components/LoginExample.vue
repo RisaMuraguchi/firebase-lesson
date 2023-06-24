aa63ef0c72a03c32c29f018447af697bf22f6538
RisaMuraguchi <126639980+RisaMuraguchi@users.noreply.github.com>
2023-06-24 15:24:25 +0900

    [Finish]9-3

--- src/components/LoginExample.vue
+++ src/components/LoginExample.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { reactive } from 'vue';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 
 const data = reactive({
   email: "",
   password: "",
   newEmail: "",
   newPassword: "",
+  isLoggedIn: false,  // ユーザーがログインしているかどうかを表すデータ
+  isLoading: true    // ユーザーのログインチェックが完了しているかを表すデータ
 });
 
 const auth = getAuth();
@@ -27,28 +29,50 @@ const signUp = () => {
       alert("ユーザー作成に成功しました！");
     })
     .catch((err) => {
-      console.log(err);
-      alert("ユーザー作成に失敗しました");
+      // console.log(err);
+      // alert("ユーザー作成に失敗しました");
+      alert(err.code);
     });
 };
+
+const logout = () => {
+  signOut(auth);
+};
+
+onAuthStateChanged(auth, (user) => {
+  data.isLoggedIn = user ? true : false;
+  data.isLoading = false;
+});
+
 </script>
 
 <template>
   <h1>Login Example</h1>
-
-  <h2>ログイン</h2>
-  <div class="container">
-
-    <div class="input-area">
-      <label for="email">メールアドレス</label>
-      <input id="email" type="text" name="email" v-model="data.email">
+  <div v-if="data.isLoading" class="loading-area">
+    Loading...
+  </div>
+  <div v-else-if="data.isLoggedIn" class="logout-area">
+    <h2>ログアウト</h2>
+    <div class="container">
+      <button @click="logout">ログアウトする</button>
     </div>
-    <div class="input-area">
-      <label for="password">パスワード</label>
-      <input id="password" type="password" name="password" v-model="data.password">
-    </div>
-    <div class="submit-area">
-      <button class="login" @click="login">ログイン</button>
+  </div>
+
+  <div v-else class="authentication-area">
+    <h2>ログイン</h2>
+    <div class="container">
+
+      <div class="input-area">
+        <label for="email">メールアドレス</label>
+        <input id="email" type="text" name="email" v-model="data.email">
+      </div>
+      <div class="input-area">
+        <label for="password">パスワード</label>
+        <input id="password" type="password" name="password" v-model="data.password">
+      </div>
+      <div class="submit-area">
+        <button class="login" @click="login">ログイン</button>
+      </div>
     </div>
   </div>
 
